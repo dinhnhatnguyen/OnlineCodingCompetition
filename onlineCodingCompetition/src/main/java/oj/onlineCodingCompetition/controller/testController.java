874c8024437a00cfb98d2a83cab102fd1d1c0d67
Nguyen Dinh Nhat <nhatnguyen188203@gmail.com>
2025-05-02 21:08:01 +0700

    implement authentication and authorization

--- onlineCodingCompetition/src/main/java/oj/onlineCodingCompetition/controller/testController.java
+++ onlineCodingCompetition/src/main/java/oj/onlineCodingCompetition/controller/testController.java
@@ -1,18 +1,35 @@
 package oj.onlineCodingCompetition.controller;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
-@RequestMapping("/")
-@RequiredArgsConstructor
-public class testController {
+@RequestMapping("/api/test")
+public class TestController {
+    @GetMapping("/all")
+    public String allAccess() {
+        return "Public Content.";
+    }
 
-    @GetMapping
-    public ResponseEntity<String> test() {
-        return ResponseEntity.ok("hehe");
+    @GetMapping("/user")
+    @PreAuthorize("hasRole('STUDENT') or hasRole('INSTRUCTOR') or hasRole('ADMIN')")
+    public String userAccess() {
+        return "User Content.";
+    }
+
+    @GetMapping("/instructor")
+    @PreAuthorize("hasRole('INSTRUCTOR') or hasRole('ADMIN')")
+    public String instructorAccess() {
+        return "Instructor Board.";
+    }
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String adminAccess() {
+        return "Admin Board.";
     }
 }

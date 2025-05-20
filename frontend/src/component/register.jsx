a0682546b3d6afbd7b5e473d3b2194703b73efe6
ANNIE <mndt8113@gmail.com>
2025-05-20 08:21:23 +0700

    edit register

--- frontend/src/component/register.jsx
+++ frontend/src/component/register.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
+import "./component/style.css";
 
 const Register = ({ switchToLogin }) => {
   return (
-    <div>
-      <h2>Create an Account</h2>
-      <p>Sign up to start solving coding challenges and join competitions.</p>
-      <input type="text" placeholder="First Name" />
-      <input type="text" placeholder="Last Name" />
-      <input type="text" placeholder="Username" />
-      <input type="email" placeholder="Email" />
-      <input type="password" placeholder="Password" />
-      <input type="password" placeholder="Confirm Password" />
-      <button className="btn">Register</button>
+    <div className="form-page">
+      <div className="form-container">
+        <h2 className="pink-gradient">Register</h2>
+        <p>Sign up to start solving coding challenges and join competitions.</p>
+        <input type="text" placeholder="First Name" />
+        <input type="text" placeholder="Last Name" />
+        <input type="text" placeholder="Username" />
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <input type="password" placeholder="Confirm Password" />
+        <button className="btn">Register</button>
       <div className="link" onClick={switchToLogin}>Already have an account? Log In</div>
+      </div>
     </div>
   );
-};
-
+}
 export default Register;

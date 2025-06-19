ed5aa7e575e2cdf61f89971604845a41f124aa2c
hoan17122003 <hoanpice32@gmail.com>
2025-06-19 10:12:07 +0700

    add service AI

---
+++ RecommendationSystem/app/main.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from app.recommendation import recommend_books as recommend_books_func
+# ,recommend_personalized_books
+from app.automation_testcase import generate_test_cases
+
+
+app = FastAPI()
+@app.post('/test')
+async def test(request: Request):
+    data = await request.json()
+    return JSONResponse(content=data)
+
+#Recommendation api endpoints
+"""
+    params : user_id, num_recommendations (optionsal, default=5 amount)
+    returns a list of recommended books based on the user's reading history.
+"""
+@app.post('/recommend_books')
+async def recommend_books(request: Request):
+    data = await request.json()
+    results = recommend_books_func(data)
+    if isinstance(results, str):
+        return JSONResponse(content={"error": results}, status_code=404)
+    return JSONResponse(content=results, status_code=200)
+
+# params : user_id, num_recommendations (optionsal, default=5 amount)
+# @app.post('/recommend_personalized_books')
+# async def recommend_personalized_books(request: Request):
+#     data = request.json()
+#     return await recommend_personalized_books(data)
+
+#default test case generation is 5 initial test cases and 5 additional test cases
+@app.post('/CreateTestCaseAutomation')
+async def create_test_case_automation(request: Request):
+    data = await request.json()
+    title = data.get('title')
+    description = data.get('description')
+    code = f"Bài_toán:{title}\nMô_tả:{description}"
+    K = data.get('K', 5)
+    return JSONResponse(content=generate_test_cases(code, K, 2), status_code=200)
+@app.get("/")
+async def root():
+    return JSONResponse(content={"message": "Welcome to the AI Service API!"}, status_code=200)
--- onlineCodingCompetition/src/main/java/oj/onlineCodingCompetition/OnlineCodingCompetitionApplication.java
+++ onlineCodingCompetition/src/main/java/oj/onlineCodingCompetition/OnlineCodingCompetitionApplication.java
@@ -14,5 +14,4 @@ public class OnlineCodingCompetitionApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineCodingCompetitionApplication.class, args);
 	}
-
 }
